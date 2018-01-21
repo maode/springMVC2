0abb9c3cc38c4dd1163a1c9bec850e1a7feb6ecf
maode <maodeboy@gmail.com>
2018-01-22 02:03:38 +0800

    添加国际化相关配置

---
+++ src/main/java/org/code0/springmvc2/configuration/MvcConfiguration.java
@@ -0,0 +1,101 @@
+package org.code0.springmvc2.configuration;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+/**  
+ * @Title: MvcConfiguration.java
+ * @Package org.code0.configuration
+ * @Description: spring mvc配置
+ * @author Code0   
+ * @date 2017年12月10日 下午3:57:46 
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.code0.springmvc2")
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+
+	/**
+	 * 配置视图解析器
+	 * @return
+	 */
+	@Bean
+	public ViewResolver viewResolver(){
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	
+	/**
+	 * 配置ResourceHandlers以服务于静态资源，如CSS/ Javascript等。
+	 * @param registry
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry)
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+	
+    /**
+     * 配置properties文件的名字【classpath:messages 指定了程序会去寻找名字为messages.properties的文件】
+     * @return
+     */
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    /**
+     * 配置默认locale和用来记录locale值的cookie
+     * @return
+     */
+    @Bean
+    public CookieLocaleResolver localeResolver(){
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.CHINESE);
+        localeResolver.setCookieName("my-locale-cookie");
+        localeResolver.setCookieMaxAge(3600);
+        return localeResolver;
+    }
+    /**
+     * 配置拦截器拦截的语言环境参数名【可选，默认为locale】
+     * @return
+     */
+    @Bean
+    public LocaleChangeInterceptor localeInterceptor(){
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");//默认是locale
+        return interceptor;
+    }
+
+    /**
+     * 将配置好的拦截器加入拦截器栈
+     * @param registry
+     * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addInterceptors(org.springframework.web.servlet.config.annotation.InterceptorRegistry)
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeInterceptor());
+    }
+}
